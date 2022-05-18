1750098b4cc8c679fe6f7727d489c9d19cc8bf2a
jhawkins <jhawkins@riotgames.com>
2022-05-18 18:07:38 -0500

    timing

---
+++ src/scripts/dashboard.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "TODO: GENERATE BUILD DASHBOARD"
--- src/scripts/send.sh
+++ src/scripts/send.sh
@@ -1,36 +1,53 @@
 #!/bin/bash
-echo Step: "${PARAM_NAME}"
+declare -a trace_array
+declare -a log_array
+
+while read p; do
+  echo "$p"
+  ts=$(echo $p | cut -d "|" -f 1)
+  log=$(echo $p | cut -d "|" -f 2)
+
+    if [ ${#trace_array[@]} -eq 0 ]; then
+        trace_array+=("$ts")
+        log_array+=("$log")
+    else
+        last_ts=${trace_array[${#trace_array[@]}-1]}
+        trace_array[${#trace_array[@]}-1]="$((ts-last_ts))"
+
+        if [ "$log" == "Build Finished" ]; then
+            echo "Skipping, build complete"
+        else
+            trace_array+=("$ts")
+            log_array+=("$log")
+        fi
+    fi
+done </tmp/trace.log
 
 CURRENT_TIME=$(date +%s)
 
-curl -vvv -k -H "Content-Type: application/json" \
--H "Api-Key: ${NR_LICENSE_KEY}" \
--X POST https://metric-api.newrelic.com/metric/v1 \
---data "[{ 
-        \"metrics\":[{ 
-           \"name\":\"build_step_time\", 
-           \"type\":\"count\", 
-           \"value\":1, 
-           \"timestamp\":${CURRENT_TIME}, 
-           \"interval.ms\": 10000,
-           \"attributes\":{
-               \"project\": \"${CIRCLE_PROJECT_REPONAME}\", 
-               \"branch\": \"${CIRCLE_BRANCH}\", 
-               \"trace_id\": \"${PARAM_TRACE_ID}\",
-               \"step\":\"${CIRCLE_NODE_INDEX}\",
-               \"build_number\":\"${CIRCLE_BUILD_NUM}\"
-               } 
-           }] 
-    }]"
+trace_array_length=${#trace_array[@]}
+for (( i=0; i<${trace_array_length}; i++ ));
+do
+  echo "index: $i, elapsed: ${trace_array[$i]}"
+  echo "index: $i, value: ${log_array[$i]}"
 
-echo "${CURRENT_TIME}|${PARAM_TRACE_ID}" >> /tmp/trace.log
-
-cat /tmp/trace.log
-
-# curl -i -H 'Content-Type: application/json' \
-#     -H "Api-Key: ${NR_LICENSE_KEY}" \
-#     -H 'Data-Format: zipkin' \
-#     -H 'Data-Format-Version: 2' \
-#     -X POST \
-#     -d '[
-#     ]' 'https://trace-api.newrelic.com/trace/v1'
+  curl -vvv -k -H "Content-Type: application/json" \
+    -H "Api-Key: ${NR_LICENSE_KEY}" \
+    -X POST https://metric-api.newrelic.com/metric/v1 \
+    --data "[{ 
+            \"metrics\":[{ 
+            \"name\":\"elapsed_build_time\", 
+            \"type\":\"gauge\", 
+            \"value\": ${trace_array[$i]}, 
+            \"timestamp\":${CURRENT_TIME}, 
+            \"interval.ms\": 10000,
+            \"attributes\":{
+                \"project\": \"${CIRCLE_PROJECT_REPONAME}\", 
+                \"branch\": \"${CIRCLE_BRANCH}\", 
+                \"trace_id\": \"${log_array[$i]}\",
+                \"step\":\"${CIRCLE_NODE_INDEX}\",
+                \"build_number\":\"${CIRCLE_BUILD_NUM}\"
+                } 
+            }] 
+        }]"
+done
