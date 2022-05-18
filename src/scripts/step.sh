18331eb17fa687fe2b9d49d0f355d3f23a279035
jhawkins <jhawkins@riotgames.com>
2022-05-18 17:11:55 -0500

    push

--- src/scripts/step.sh
+++ src/scripts/step.sh
@@ -8,10 +8,10 @@ curl -vvv -k -H "Content-Type: application/json" \
 --data "[{ 
         \"metrics\":[{ 
            \"name\":\"build_step_time\", 
-           \"type\":\"gauge\", 
+           \"type\":\"count\", 
            \"value\":1, 
            \"timestamp\":${CURRENT_TIME}, 
-           \"interval.ms\": 000000,
+           \"interval.ms\": 10000,
            \"attributes\":{
                \"project\": \"${CIRCLE_PROJECT_REPONAME}\", 
                \"branch\": \"${CIRCLE_BRANCH}\", 
